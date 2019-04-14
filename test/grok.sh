a321f7f5ac3025dfd2a7a8d23c5b24e5514d8c2f
orhmeh09 <altan@imap.cc>
2019-04-14 01:08:03 -0700

    Updated to reflect current ElasticIp for testing

--- test/grok.sh
+++ test/grok.sh
@@ -2,8 +2,7 @@
 
 URL=${BEEVES_SERVER_URL:-localhost:8337}
 
-
-if [ $1 ]; then
+if [[ "$#" -gt 0 ]]; then
     echo "Connecting to $URL"
-    curl --data "{\"q\":\"$1\"}" -v --request POST -H "Content-Type: application/json" --url localhost:8337/grok  --cookie "BEEVES_KEY=$BEEVES_KEY"
+    curl  -d "{\"q\" : \"$1\"}"   -v --request POST -H "Content-Type: application/json" --url localhost:8337/grok  --cookie "BEEVES_KEY=$BEEVES_KEY"
 fi
